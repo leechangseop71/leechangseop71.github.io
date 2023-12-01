7802d828b969b296474fc0b8e8281d92c055fc48
leechangseop71 <haseuki71@gmail.com>
2023-12-01 23:13:38 +0900

    수정

--- js/script.js
+++ js/script.js
@@ -78,4 +78,4 @@ ScrollReveal({
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
-ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content h3, .about-content', { origin: 'right' });
