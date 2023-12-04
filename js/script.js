4bc8534ae9a4c7624b8be26d976c75920fad7dd6
leechangseop71 <haseuki71@gmail.com>
2023-12-05 08:55:05 +0900

    수정

--- js/script.js
+++ js/script.js
@@ -80,57 +80,15 @@ ScrollReveal().reveal('.home-img img, .skill-container, .portfolio-box, .testimo
 ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
 ScrollReveal().reveal('.home-content h3, .about-content', { origin: 'right' });
 
-(function() {
-  
-    var SkillsBar = function( bars ) {
-      this.bars = document.querySelectorAll( bars );
-      if( this.bars.length > 0 ) {
-        this.init();
-      } 
-    };
-    
-    SkillsBar.prototype = {
-      init: function() {
-        var self = this;
-        self.index = -1;
-        self.timer = setTimeout(function() {
-          self.action();
-        }, 500);
-        
-        
-      },
-      select: function( n ) {
-        var self = this,
-          bar = self.bars[n];
-          
-          if( bar ) {
-            var width = bar.parentNode.dataset.percent;
-          
-            bar.style.width = width;
-            bar.parentNode.classList.add( "complete" ); 
-          }
-      },
-      action: function() {
-        var self = this;
-        self.index++;
-        if( self.index == self.bars.length ) {
-          clearTimeout( self.timer );
-        } else {
-          self.select( self.index );  
-        }
-        
-        setTimeout(function() {
-          self.action();
-        },500);
-      }
-    };
-    
-    window.SkillsBar = SkillsBar;
-    
-  })();
-  
-  (function() {
-    document.addEventListener( "DOMContentLoaded", function() {
-      var skills = new SkillsBar( ".skillbar-bar" );
-    });
-  })();
+// Get all skill bars with their respective percentages
+const skillBars = document.querySelectorAll('.skillbar');
+
+// Loop through each skill bar and set the width based on the data-percent attribute
+skillBars.forEach((bar) => {
+  const percent = bar.getAttribute('data-percent');
+  const skillBar = bar.querySelector('.skillbar-bar');
+  const skillBarPercent = bar.querySelector('.skill-bar-percent');
+
+  skillBar.style.width = percent;
+  skillBarPercent.innerText = percent;
+});
